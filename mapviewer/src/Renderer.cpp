03c9c86935456de822f37158fdbd7000c469dc17
Thomas Fjellstrom <thomas@fjellstrom.ca>
2014-10-04 09:30:08 -0600

    only load part of map

--- mapviewer/src/Renderer.cpp
+++ mapviewer/src/Renderer.cpp
@@ -641,7 +641,7 @@ bool Renderer::isChunkVisible(Vector3D origin, Vector3D pos)
 {
 	Vector3D dist = pos - origin;
 	
-	if(dist.magnitude() <= 4.0)
+	if(dist.magnitude() <= 2.0)
 		return true;
 	
 	return false;
